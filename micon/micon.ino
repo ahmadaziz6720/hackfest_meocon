0cbe3ba38834d99040800da83a8826e4452f65d4
ahmadaziz6720 <a715210@gmail.com>
2022-03-18 21:15:02 +0700

    flowchart

---
+++ micon/micon.ino
@@ -0,0 +1,16 @@
+#include <Adafruit_MLX90614.h>
+
+#define relay 2
+#define ir_in 4
+#define ir_out 7
+#define ir_sanitizer 12
+#define servo_sanitizer 3
+#define servo_pintu 5
+
+void setup() {
+
+}
+
+void loop() {
+
+}
