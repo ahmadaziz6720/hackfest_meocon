d95c46876017de033affef953938d5219a03abba
ahmadaziz6720 <a715210@gmail.com>
2022-03-23 16:49:21 +0700

    sensor

--- src/main/tes_sensor/tes_sensor.ino
+++ src/main/tes_sensor/tes_sensor.ino
@@ -4,23 +4,33 @@
 #define ir_in 4
 #define ir_out 7
 #define ir_hs 12
-#define laser 8
-
-bool idle = true;
-bool mask = false;
-int state = 0;
-float suhu = 0;
-int count_ppl = 0;
-
-unsigned long timer;
-
+//#define laser 8
 
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(relay_pump, OUTPUT);
+  pinMode(relay_lock, OUTPUT);
+  pinMode(ir_in, INPUT);
+  pinMode(ir_out, INPUT);
+  pinMode(ir_hs, INPUT);
+  Serial.begin(9600);
+//  pinMode(laser, OUTPUT);
 }
 
 void loop() {
+  Serial.print("ir_in:");
+  Serial.print(digitalRead(ir_in));
+  Serial.print("     ir_out:");
+  Serial.print(digitalRead(ir_out));
+  Serial.print("     ir_hs:");
+  Serial.println(digitalRead(ir_hs));
+
+  if(digitalRead(ir_in)==LOW){
+    digitalWrite(relay_pump, HIGH);
+    digitalWrite(relay_lock, LOW);
+  }else if(digitalRead(ir_in)==HIGH){
+    digitalWrite(relay_pump, LOW);
+    digitalWrite(relay_lock, HIGH);
+  }
   // put your main code here, to run repeatedly:
 
 }
