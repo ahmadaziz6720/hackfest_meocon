005c22e892995743e4cc55093fd8ee722efd19cf
ahmadaziz6720 <a715210@gmail.com>
2022-03-31 05:22:06 +0700

    python to firebase

---
+++ src/IoT/python-firebase.py
@@ -0,0 +1,26 @@
+# referensi: https://github.com/RishabKattimani/FirebaseDBToPython.git
+# pip install pyrebase
+
+import pyrebase
+
+config = {
+    "apiKey": "AIzaSyCTARMTNV8J6w24RJoR91RN8D07hZxkWZ0",
+    "authDomain": "approkes.firebaseapp.com",
+    "databaseURL": "https://approkes-default-rtdb.asia-southeast1.firebasedatabase.app",
+    "projectId": "approkes",
+    "storageBucket": "approkes.appspot.com",
+    "messagingSenderId": "179843523866",
+    "appId": "1:179843523866:web:52a90ac9dc0a7b952ff8a3"
+}
+
+firebase = pyrebase.initialize_app(config)
+database = firebase.database()
+
+data = {"Name": "liza", "MSG": "HAII LIZA", "Catatan": "Ini dari Python"}
+
+database.child("test").set(data)
+
+for i in database.child("test").get().each():
+    print(i.val())
+
+
